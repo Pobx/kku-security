fddc2f92413af6e35a40d7da9d4beda337f6fb18
Pobx <geidtiphong@gmail.com>
2018-08-30 21:42:22 +0700

    add table-responsive

--- application/views/accidents_table.php
+++ application/views/accidents_table.php
@@ -18,9 +18,62 @@
 
 			<br />
 
-			<table class="table table-bordered table-striped mydataTable">
-				<thead>
-					<tr>
+			<div class="table-responsive">
+				<table class="table table-bordered table-striped mydataTable">
+					<thead>
+						<tr>
+							<?php foreach ($header_columns as $key => $value)
+{
+    ?>
+							<th class="text-center">
+								<?php echo $value; ?>
+							</th>
+							<?php }?>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($results as $key => $value)
+{
+    ?>
+						<tr>
+							<td class="text-center">
+								<?php echo $value['accident_date'];?>
+							</td>
+							<td class="text-center">
+								<?php echo $value['period_time']; ?>
+							</td>
+							<td class="text-center">
+								<?php echo $value['place']; ?>
+							</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">
+								<?php echo $value['accident_cause']; ?>
+							</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+							<td class="text-center">XXX</td>
+
+							<td class="text-center">
+								<a href="<?php echo $link_go_to_form . '/' . $value['id']; ?>" class="btn btn-warning">
+									<i class="fa fa-pencil"></i>
+								</a>
+							</td>
+							<td class="text-center">
+								<a href="javascript:removeItem('<?php echo $value['id']; ?>', '<?php echo $link_go_to_remove; ?>')" class="btn btn-danger">
+									<i class="fa fa-trash-o"></i>
+								</a>
+							</td>
+
+						</tr>
+						<?php }?>
+					</tbody>
+					<tfoot>
 						<?php foreach ($header_columns as $key => $value)
 {
     ?>
@@ -28,60 +81,10 @@
 							<?php echo $value; ?>
 						</th>
 						<?php }?>
-					</tr>
-				</thead>
-				<tbody>
-					<?php foreach ($results as $key => $value)
-{
-    ?>
-					<tr>
-						<td class="text-center">
-							<?php echo $value['accident_date'];?>
-						</td>
-						<td class="text-center">
-							<?php echo $value['period_time']; ?>
-						</td>
-						<td class="text-center">
-							<?php echo $value['place']; ?>
-						</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">
-							<?php echo $value['accident_cause']; ?>
-						</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
-						<td class="text-center">XXX</td>
+					</tfoot>
+				</table>
+			</div>
 
-						<td class="text-center">
-							<a href="<?php echo $link_go_to_form . '/' . $value['id']; ?>" class="btn btn-warning">
-								<i class="fa fa-pencil"></i>
-							</a>
-						</td>
-						<td class="text-center">
-							<a href="javascript:removeItem('<?php echo $value['id']; ?>', '<?php echo $link_go_to_remove; ?>')" class="btn btn-danger">
-								<i class="fa fa-trash-o"></i>
-							</a>
-						</td>
-
-					</tr>
-					<?php }?>
-				</tbody>
-				<tfoot>
-					<?php foreach ($header_columns as $key => $value)
-{
-    ?>
-					<th class="text-center">
-						<?php echo $value; ?>
-					</th>
-					<?php }?>
-				</tfoot>
-			</table>
 		</div>
 
 		<div class="box-footer">
