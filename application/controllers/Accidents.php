bdd34c99f66b0f2e31e0cbc1ffc1d84e003beb0c
Pobx <geidtiphong@gmail.com>
2018-08-30 13:20:34 +0700

    create class: Accidents

---
+++ application/controllers/Accidents.php
@@ -0,0 +1,109 @@
+<?php
+defined('BASEPATH') || exit('No direct script access allowed');
+
+class Accidents extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('accidents_model');
+        $this->load->library('Date_libs');
+    }
+
+    private $head_topic_label           = 'สถิติอุบัติเหตุ';
+    private $head_sub_topic_label_table = 'รายการ สถิติอุบัติเหตุ';
+    private $head_sub_topic_label_form  = 'ฟอร์มบันทึกข้อมูล สถิติอุบัติเหตุ';
+    private $header_columns             = array('วันที่', 'ช่วงเวลา', 'สถานที่เกิดเหตุ', 'รถยนต์', 'รถจักรยานยนต์', 'รถที่เกิดเหตุ', 'สาเหตุ', 'บาดเจ็บ', 'เสียชีวิต', 'ผู้ประสบเหตุ / คู่กรณี', 'หน่วยงาน', 'บุคลากร', 'นักศึกษา', 'บุคคลภายใน', 'แก้ไข', 'ลบ');
+    private $success_message            = 'บันทึกข้อมูลสำเร็จ';
+    private $warning_message            = 'ไม่สามารถทำรายการ กรุณลองใหม่อีกครั้ง';
+    private $danger_message             = 'ลบข้อมูลสำเร็จ';
+
+    public function index()
+    {
+        $data['head_topic_label'] = $this->head_topic_label;
+        $data['head_sub_topic_label'] = $this->head_sub_topic_label_table;
+        $data['link_go_to_form'] = site_url('accidents/form_store');
+        $data['link_go_to_remove'] = site_url('accidents/remove');
+        $data['header_columns'] = $this->header_columns;
+
+        $qstr = array('status !=' => 'disabled');
+        $results = $this->accidents_model->all($qstr);
+        $data['results'] = $results['results'];
+        $data['fields'] = $results['fields'];
+        $data['content'] = 'accidents_table';
+
+        // echo "<pre>", print_r($data['results']); exit();
+        $this->load->view('template_layout', $data);
+    }
+
+    public function form_store()
+    {
+        $id = $this->uri->segment(3);
+
+        $data = $this->find($id);
+        $data['head_topic_label'] = $this->head_topic_label;
+        $data['head_sub_topic_label'] = $this->head_sub_topic_label_form;
+        $data['link_back_to_table'] = site_url('accidents');
+        $data['form_submit_data_url'] = site_url('accidents/store');
+
+        $data['content'] = 'accidents_form_store';
+
+        // echo "<pre>", print_r($data); exit();
+        $this->load->view('template_layout', $data);
+    }
+
+    public function store()
+    {
+        $inptus = $this->input->post();
+        $inptus['inspect_date'] = $this->date_libs->set_date_th($inptus['inspect_date']);
+        $results = $this->accidents_model->store($inptus);
+
+        $alert_type = ($results['query'] ? 'success' : 'warning');
+        $alert_icon = ($results['query'] ? 'check' : 'warning');
+        $alert_message = ($results['query'] ? $this->success_message : $this->warning_message);
+        $this->session->set_flashdata('alert_type', $alert_type);
+        $this->session->set_flashdata('alert_icon', $alert_icon);
+        $this->session->set_flashdata('alert_message', $alert_message);
+
+        redirect('accidents');
+    }
+
+    private function find($id = 0)
+    {
+        $results = $this->accidents_model->find($id);
+        $values = $results['results'];
+        $fields = $results['fields'];
+        $rows = $results['rows'];
+        $data = array();
+
+        foreach ($fields as $key => $value)
+        {
+            if ($rows <= 0)
+            {
+                $data[$value] = '';
+            }
+            else
+            {
+                $data[$value] = $values->$value;
+            }
+        }
+
+        return $data;
+    }
+
+    public function remove()
+    {
+        $id = $this->uri->segment(3);
+        $results = $this->accidents_model->remove($id);
+
+        $alert_type = ($results['query'] ? 'danger' : 'warning');
+        $alert_icon = ($results['query'] ? 'trash' : 'warning');
+        $alert_message = ($results['query'] ? $this->danger_message : $this->warning_message);
+        $this->session->set_flashdata('alert_type', $alert_type);
+        $this->session->set_flashdata('alert_icon', $alert_icon);
+        $this->session->set_flashdata('alert_message', $alert_message);
+
+        redirect('accidents');
+    }
+}
---
+++ application/models/Accidents_model.php
@@ -0,0 +1,76 @@
+<?php
+defined('BASEPATH') || exit('No direct script access allowed');
+
+class Student_do_not_wear_helmet_model extends CI_Model
+{
+
+    private $table = 'student_do_not_wear_helmet';
+    private $id    = 'id';
+    private $items = '
+    id,
+    DATE_FORMAT(DATE_ADD(accident_date, INTERVAL 543 YEAR),"%d/%m/%Y") as accident_date,
+    period_time,
+    place,
+    status
+    ';
+
+    public function all($qstr = '')
+    {
+        if (isset($qstr) && !empty($qstr))
+        {
+            $this->db->where($qstr);
+        }
+
+        $query = $this->db->select($this->items)->from($this->table)->get();
+
+        $results['results'] = $query->result_array();
+        $results['rows'] = $query->num_rows();
+        $results['fields'] = $query->list_fields();
+
+        return $results;
+    }
+
+    public function find($id)
+    {
+        $query = $this->db->select($this->items)->from($this->table)->where('id', $id)->get();
+
+        $results['rows'] = $query->num_rows();
+        $results['results'] = $query->first_row();
+        $results['fields'] = $query->list_fields();
+
+        return $results;
+    }
+
+    public function store($inputs)
+    {
+        // echo "<pre>", print_r($inputs); exit();
+        if ($inputs['id'] != '')
+        {
+            $inputs['updated'] = date('Y-m-d H:i:s');
+            $results['query'] = $this->db->where($this->id, $inputs['id'])->update($this->table, $inputs);
+            $results['lastID'] = $inputs['id'];
+        }
+        else
+        {
+            $inputs['created'] = date('Y-m-d H:i:s');
+            $results['query'] = $this->db->insert($this->table, $inputs);
+            $results['lastID'] = $this->db->insert_id();
+        }
+
+        return $results;
+    }
+
+    public function remove($id)
+    {
+        $inputs = array(
+            'id'      => $id,
+            'updated' => date('Y-m-d H:i:s'),
+            'status'  => 'disabled',
+        );
+
+        $results['query'] = $this->db->where($this->id, $inputs['id'])->update($this->table, $inputs);
+
+        return $results;
+    }
+
+}
