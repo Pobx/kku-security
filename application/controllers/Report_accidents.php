3e783204b17ce9badb4f53b23311a2d0008396c8
Pobx <geidtiphong@gmail.com>
2018-08-31 10:09:21 +0700

    create class: Report_accidents

---
+++ application/controllers/Report_accidents.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') || exit('No direct script access allowed');
+
+class Report_accidents extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('Accidents_model');
+        $this->load->model('Accidents_vehicles_model');
+        $this->load->model('Accidents_peoples_model');
+
+        $this->load->library('Date_libs');
+    }
+
+    private $head_topic_label           = 'สถิติอุบัติเหตุ';
+    private $head_sub_topic_label_table = 'รายงาน สถิติอุบัติเหตุ';
+    private $header_columns             = array('วันที่', 'ช่วงเวลา', 'สถานที่เกิดเหตุ', 'รถยนต์', 'รถจักรยานยนต์', 'รถที่เกิดเหตุ', 'สาเหตุ', 'บาดเจ็บ', 'เสียชีวิต', 'ผู้ประสบเหตุ / คู่กรณี', 'หน่วยงาน', 'บุคลากร', 'นักศึกษา', 'บุคคลภายใน');
+  
+
+    public function index()
+    {
+        $data['head_topic_label'] = $this->head_topic_label;
+        $data['head_sub_topic_label'] = $this->head_sub_topic_label_table;
+      
+        $data['header_columns'] = $this->header_columns;
+
+        $qstr = array('status !=' => 'disabled');
+        $results = $this->Accidents_model->all($qstr);
+        $data['results'] = $results['results'];
+        $data['fields'] = $results['fields'];
+        $data['content'] = 'report_accidents_table';
+
+        // echo "<pre>", print_r($data['results']); exit();
+        $this->load->view('template_layout', $data);
+    }
+
+}
--- application/views/sidebar_admin.php
+++ application/views/sidebar_admin.php
@@ -28,14 +28,35 @@
 
 <li class="treeview">
 	<a href="#">
-		<i class="fa fa-dashboard"></i> <span>รายงาน</span>
+		<i class="fa fa-book"></i> <span>รายงาน</span>
 		<span class="pull-right-container">
 			<i class="fa fa-angle-left pull-right"></i>
 		</span>
 	</a>
 
 	<ul class="treeview-menu">
-		<li class=""><a href="#"><i class="fa fa-circle-o"></i> รายงาน v1</a></li>
-		<li><a href="#"><i class="fa fa-circle-o"></i> รายงาน v2</a></li>
+		<li>
+			<a href="<?php echo site_url('report_accidents');?>"><i class="fa fa-warning"></i> <span>สถิติอุบัติเหตุ</span></a>
+		</li>
+
+		<li>
+			<a href="<?php echo site_url('#');?>"><i class="fa fa-home"></i> <span>โครงการฝากบ้าน</span></a>
+		</li>
+
+		<li>
+			<a href="<?php echo site_url('#');?>"><i class="fa fa-key"></i> <span>สถิติการลืมกุญแจ</span></a>
+		</li>
+
+		<li>
+			<a href="<?php echo site_url('#');?>"><i class="fa fa-home"></i> <span>สถิติเหตุทรัพย์งัดที่พักอาศัย</span></a>
+		</li>
+
+		<li>
+			<a href="<?php echo site_url('#');?>"><i class="fa fa-motorcycle"></i> <span>สถิติงัดเบาะรถจักยานยนต์</span></a>
+		</li>
+
+		<li>
+			<a href="<?php echo site_url('#');?>"><i class="fa fa-motorcycle"></i> <span>สถิติไม่สวมหมวกนิรภัย</span></a>
+		</li>
 	</ul>
 </li>
