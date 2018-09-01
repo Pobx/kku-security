4225311cf436febebc8afaa95a3ab08311023e4b
Pobx <geidtiphong@gmail.com>
2018-09-01 12:13:25 +0700

    create label for earch accidents

--- application/views/dashboard_admin_top_ten_departments_panel.php
+++ application/views/dashboard_admin_top_ten_departments_panel.php
@@ -7,65 +7,32 @@
 			</button>
 		</div>
 	</div>
-	<!-- /.box-header -->
+
 	<div class="box-body">
+
 		<ul class="products-list product-list-in-box">
 			<li class="item">
 				<div class="product-img">
 					<img src="<?php echo base_url('dist/img/default-50x50.gif');?>" alt="Product Image">
 				</div>
 				<div class="product-info">
-					<a href="javascript:void(0)" class="product-title">Samsung TV
-						<span class="label label-warning pull-right">$1800</span></a>
+					<a href="javascript:void(0)" class="product-title">คณะทันตแพทยศาสตร์</a>
 					<span class="product-description">
-						Samsung 32" 1080p 60Hz LED Smart HDTV.
+						<span class="label label-danger"><i class="fa fa-motorcycle"></i> 25 ครั้ง</span>
+						<span class="label label-success"><i class="fa fa-home"></i> 14 ครั้ง</span>
+						<span class="label label-info"><i class="fa fa-key"></i> 50 ครั้ง</span>
+						<span class="label label-warning"><i class="fa fa-home"></i> 43 ครั้ง</span>
+						<span class="label" style="background-color: #ff851b !important;">
+							<i class="fa fa-motorcycle"></i> 102 ครั้ง</span>
+						<span class="label label-primary"><i class="fa fa-motorcycle"></i> 299 ครั้ง</span>
 					</span>
 				</div>
 			</li>
-			<!-- /.item -->
-			<li class="item">
-				<div class="product-img">
-					<img src="<?php echo base_url('dist/img/default-50x50.gif');?>" alt="Product Image">
-				</div>
-				<div class="product-info">
-					<a href="javascript:void(0)" class="product-title">Bicycle
-						<span class="label label-info pull-right">$700</span></a>
-					<span class="product-description">
-						26" Mongoose Dolomite Men's 7-speed, Navy Blue.
-					</span>
-				</div>
-			</li>
-			<!-- /.item -->
-			<li class="item">
-				<div class="product-img">
-					<img src="<?php echo base_url('dist/img/default-50x50.gif');?>" alt="Product Image">
-				</div>
-				<div class="product-info">
-					<a href="javascript:void(0)" class="product-title">Xbox One <span class="label label-danger pull-right">$350</span></a>
-					<span class="product-description">
-						Xbox One Console Bundle with Halo Master Chief Collection.
-					</span>
-				</div>
-			</li>
-			<!-- /.item -->
-			<li class="item">
-				<div class="product-img">
-					<img src="<?php echo base_url('dist/img/default-50x50.gif');?>" alt="Product Image">
-				</div>
-				<div class="product-info">
-					<a href="javascript:void(0)" class="product-title">PlayStation 4
-						<span class="label label-success pull-right">$399</span></a>
-					<span class="product-description">
-						PlayStation 4 500GB Console (PS4)
-					</span>
-				</div>
-			</li>
-			<!-- /.item -->
 		</ul>
 	</div>
-	<!-- /.box-body -->
+
 	<div class="box-footer text-center">
 		<!-- <a href="javascript:void(0)" class="uppercase">View All Products</a> -->
 	</div>
-	<!-- /.box-footer -->
+
 </div>
