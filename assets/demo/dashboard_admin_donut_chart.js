96c7933460612c11048e41a6384c3babcabbceef
Pobx <geidtiphong@gmail.com>
2018-09-01 11:47:49 +0700

    edit data on pie chart OK

--- assets/demo/dashboard_admin_donut_chart.js
+++ assets/demo/dashboard_admin_donut_chart.js
@@ -5,40 +5,40 @@
 	var pieChartCanvas = $('#pieChart').get(0).getContext('2d');
 	var pieChart = new Chart(pieChartCanvas);
 	var PieData = [{
-			value: 700,
-			color: '#f56954',
-			highlight: '#f56954',
-			label: 'Chrome'
+			value: 25,
+			color: '#dd4b39',
+			highlight: '#dd4b39',
+			label: 'สถิติอุบัติเหตุ'
 		},
 		{
-			value: 500,
+			value: 14,
 			color: '#00a65a',
 			highlight: '#00a65a',
-			label: 'IE'
+			label: 'โครงการฝากบ้าน'
 		},
 		{
-			value: 400,
-			color: '#f39c12',
-			highlight: '#f39c12',
-			label: 'FireFox'
-		},
-		{
-			value: 600,
+			value: 50,
 			color: '#00c0ef',
 			highlight: '#00c0ef',
-			label: 'Safari'
+			label: 'สถิติการลืมกุญแจ'
 		},
 		{
-			value: 300,
-			color: '#3c8dbc',
-			highlight: '#3c8dbc',
-			label: 'Opera'
+			value: 43,
+			color: '#f39c12',
+			highlight: '#f39c12',
+			label: 'สถิติเหตุทรัพย์งัดที่พักอาศัย'
 		},
 		{
-			value: 100,
-			color: '#d2d6de',
-			highlight: '#d2d6de',
-			label: 'Navigator'
+			value: 102,
+			color: '#ff851b',
+			highlight: '#ff851b',
+			label: 'สถิติงัดเบาะรถจักยานยนต์'
+		},
+		{
+			value: 299,
+			color: '#0073b7',
+			highlight: '#0073b7',
+			label: 'สถิติไม่สวมหมวกนิรภัย'
 		}
 	];
 	var pieOptions = {
@@ -63,9 +63,9 @@
 		// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
 		maintainAspectRatio: false,
 		// String - A legend template
-		legendTemplate: '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<segments.length; i++){%><li><span style=\'background-color:<%=segments[i].fillColor%>\'></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
+		// legendTemplate: '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<segments.length; i++){%><li><span style=\'background-color:<%=segments[i].fillColor%>\'></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
 		// String - A tooltip template
-		tooltipTemplate: '<%=value %> <%=label%> users'
+		// tooltipTemplate: '<%=value %> <%=label%> users'
 	};
 	// Create pie or douhnut chart
 	// You can switch between pie and douhnut using the method below.
