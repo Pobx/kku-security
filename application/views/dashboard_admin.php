73a48f6c716e4318c8ab52cdadcbaced77d1f103
Pobx <geidtiphong@gmail.com>
2018-09-01 13:23:23 +0700

    edit data on top four box

--- application/views/dashboard_admin.php
+++ application/views/dashboard_admin.php
@@ -1,64 +1,59 @@
 <div class="row">
 	<div class="col-lg-3 col-xs-6">
-		<!-- small box -->
 		<div class="small-box bg-aqua">
 			<div class="inner">
-				<h3>150</h3>
+				<h3>356</h3>
 
-				<p>New Orders</p>
+				<p>เหตุการณ์</p>
 			</div>
 			<div class="icon">
-				<i class="ion ion-bag"></i>
+				<i class="fa fa-exclamation-triangle"></i>
 			</div>
-			<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+			<a href="#" class="small-box-footer">&nbsp;</a>
 		</div>
 	</div>
-	<!-- ./col -->
-	<div class="col-lg-3 col-xs-6">
-		<!-- small box -->
-		<div class="small-box bg-green">
-			<div class="inner">
-				<h3>53<sup style="font-size: 20px">%</sup></h3>
 
-				<p>Bounce Rate</p>
-			</div>
-			<div class="icon">
-				<i class="ion ion-stats-bars"></i>
-			</div>
-			<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-		</div>
-	</div>
-	<!-- ./col -->
 	<div class="col-lg-3 col-xs-6">
-		<!-- small box -->
-		<div class="small-box bg-yellow">
-			<div class="inner">
-				<h3>44</h3>
-
-				<p>User Registrations</p>
-			</div>
-			<div class="icon">
-				<i class="ion ion-person-add"></i>
-			</div>
-			<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-		</div>
-	</div>
-	<!-- ./col -->
-	<div class="col-lg-3 col-xs-6">
-		<!-- small box -->
 		<div class="small-box bg-red">
 			<div class="inner">
-				<h3>65</h3>
+				<h3>25</h3>
 
-				<p>Unique Visitors</p>
+				<p>สถิติอุบัติเหตุ</p>
 			</div>
 			<div class="icon">
-				<i class="ion ion-pie-graph"></i>
+				<i class="fa fa-motorcycle"></i>
 			</div>
-			<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+			<a href="#" class="small-box-footer">&nbsp;</a>
 		</div>
 	</div>
-	<!-- ./col -->
+
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-yellow">
+			<div class="inner">
+				<h3>34</h3>
+
+				<p>บาดเจ็บ</p>
+			</div>
+			<div class="icon">
+				<i class="fa fa-wheelchair"></i>
+			</div>
+			<a href="#" class="small-box-footer">&nbsp;</a>
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-red">
+			<div class="inner">
+				<h3>2</h3>
+
+				<p>เสียชีวิต</p>
+			</div>
+			<div class="icon">
+				<i class="fa fa-user-times"></i>
+			</div>
+			<a href="#" class="small-box-footer">&nbsp;</a>
+		</div>
+	</div>
 </div>
 
 <div class="row">
