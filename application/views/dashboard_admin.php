7cc01a079816a3c616823a9db39e0198e8fb802b
Pobx <geidtiphong@gmail.com>
2018-09-03 21:19:18 +0700

    comment table of dead and dashboard_bar_chart

--- application/views/dashboard_admin.php
+++ application/views/dashboard_admin.php
@@ -76,9 +76,9 @@
 
 <div class="row">
 	<div class="col-md-6">
-		<?php $this->load->view('dashboard_admin_table_dead_of_year_panel');?>
+		<?php //$this->load->view('dashboard_admin_table_dead_of_year_panel');?>
 	</div>
 	<div class="col-md-6">
-		<?php $this->load->view('dashboard_admin_bar_chart_monthly');?>
+		<?php //$this->load->view('dashboard_admin_bar_chart_monthly');?>
 	</div>
 </div>
