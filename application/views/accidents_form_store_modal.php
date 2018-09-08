40a776e761e181121118354fb7e19f6a7ec09a45
Pobx <geidtiphong@gmail.com>
2018-09-08 15:56:38 +0700

    add vehicles_information

--- application/views/accidents_form_store_modal.php
+++ application/views/accidents_form_store_modal.php
@@ -1,14 +1,19 @@
 <div class="modal fade" id="modal-default">
-	<div class="modal-dialog">
+	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title">Default Modal</h4>
+				<h4 class="modal-title">
+					<?php echo $head_topic_participate_label; ?>
+				</h4>
 			</div>
 			<div class="modal-body">
-				<?php $this->load->view('header_form_submit_data');?>
-				<?php $this->load->view('accidents_peoples_information_form_store');?>
+				<?php 
+          $this->load->view('header_form_submit_data');
+          $this->load->view('accidents_peoples_information_form_store');
+          $this->load->view('accidents_vehicles_information_form_store');
+        ?>
 				</form>
 			</div>
 			<div class="modal-footer">
