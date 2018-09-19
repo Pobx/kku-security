83e1a75e54f13fd2abd99621fa21b04e08a5905d
Pobx <geidtiphong@gmail.com>
2018-09-20 06:15:26 +0700

    get add method: all to Evaluation_model

--- application/controllers/Report_evaluations.php
+++ application/controllers/Report_evaluations.php
@@ -7,9 +7,10 @@ class Report_evaluations extends CI_Controller
     {
         parent::__construct();
 
-        $this->load->model('Redbox_inspect_transaction_model');
+        $this->load->model('Evaluation_model');
         $this->load->library('Date_libs');
         $this->load->library('FilterBarChartData');
+        $this->load->library('FilterPeoples');
     }
 
     private $head_topic_label           = 'สถิติกล่องแดง';
@@ -19,8 +20,11 @@ class Report_evaluations extends CI_Controller
     public function index()
     {
         $inputs = $this->input->post();
-        $data['start_date'] = (isset($inputs['start_date']) ? $inputs['start_date'] : $this->date_libs->get_date_th(date('Y-m-d')));
-        $data['end_date'] = (isset($inputs['end_date']) ? $inputs['end_date'] : $this->date_libs->get_date_th(date('Y-m-d')));
+        $data['start_date'] = '01/01/2561';
+        $data['end_date'] = '01/01/2562';
+        
+        // $data['start_date'] = (isset($inputs['start_date']) ? $inputs['start_date'] : $this->date_libs->get_date_th(date('Y-m-d')));
+        // $data['end_date'] = (isset($inputs['end_date']) ? $inputs['end_date'] : $this->date_libs->get_date_th(date('Y-m-d')));
 
         $data['head_topic_label'] = $this->head_topic_label;
         $data['head_sub_topic_label'] = $this->head_sub_topic_label_table;
@@ -29,9 +33,9 @@ class Report_evaluations extends CI_Controller
         $data['link_excel'] = site_url('report_evaluations/export_excel');
 
         $qstr = array(
-            'DATE(redbox_inspect_transaction.inspect_date) >=' => $this->date_libs->set_date_th($data['start_date']),
-            'DATE(redbox_inspect_transaction.inspect_date) <=' => $this->date_libs->set_date_th($data['end_date']),
-            'redbox_inspect_transaction.status'                => 'active',
+            'DATE(evaluations.eval_date) >=' => $this->date_libs->set_date_th($data['start_date']),
+            'DATE(evaluations.eval_date) <=' => $this->date_libs->set_date_th($data['end_date']),
+            'evaluations.status'                => 'active',
         );
 
         $sess_inputs = array(
@@ -48,7 +52,7 @@ class Report_evaluations extends CI_Controller
         $data['fields'] = $results['fields'];
         $data['content'] = 'report_evaluations_table';
 
-        // echo "<pre>", print_r($data['results']); exit();
+        echo "<pre>", print_r($data['results']); exit();
         $this->load->view('template_layout', $data);
     }
 
--- application/models/Evaluation_model.php
+++ application/models/Evaluation_model.php
@@ -4,6 +4,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Evaluation_model extends CI_Model {
 
   private $table='evaluations';
+  private $id    = 'id';
+  private $items = '
+    id, 
+    gender,
+    age,
+    personal_id,
+    department_id,
+    performance,
+    performance,
+    success,
+    timeline,
+    service_clear,
+    materials,
+    servicemind,
+    communication,
+    knowlage,
+    questions,
+    followup,
+    comment,
+    gender,
+    
+  ';
+
+  public function all($qstr = '')
+    {
+        if (isset($qstr) && !empty($qstr))
+        {
+            $this->db->where($qstr);
+        }
+
+        $query = $this->db->select($this->items)->from($this->table)->get();
+
+        $results['results'] = $query->result_array();
+        $results['rows'] = $query->num_rows();
+        $results['fields'] = $query->list_fields();
+
+        return $results;
+    }
 
   public function store($inputs)
 	{
