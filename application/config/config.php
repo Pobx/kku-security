bb09a624ad01a37b3fe2618ec010f59f1218e9f0
pobx <pop@pipdee.com>
2018-09-21 08:28:16 +0700

    add link setting users

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') || exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = 'http://localhost:9001/kku-security';
-  // $config['base_url'] = 'http://localhost/kku-security';
+// $config['base_url'] = 'http://localhost:9001/kku-security';
+  $config['base_url'] = 'http://localhost/kku-security';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/sidebar_admin.php
+++ application/views/sidebar_admin.php
@@ -94,3 +94,18 @@
 		</li>
 	</ul>
 </li>
+
+<li class="treeview">
+	<a href="#">
+		<i class="fa fa-book"></i> <span>Setting</span>
+		<span class="pull-right-container">
+			<i class="fa fa-angle-left pull-right"></i>
+		</span>
+	</a>
+
+	<ul class="treeview-menu">
+		<li>
+			<a href="<?php echo site_url('evaluation'); ?>"><i class="fa fa-users"></i> <span>ผู้ใช้งาน</span></a>
+		</li>
+	</ul>
+</li>
