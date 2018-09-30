a4d38e756465bef5d165cb43014cd8fd40b43c8a
Pobx <geidtiphong@gmail.com>
2018-09-30 08:18:16 +0700

    add car information to steal_motocycle_form

--- application/views/steal_motocycle_form_store.php
+++ application/views/steal_motocycle_form_store.php
@@ -56,6 +56,39 @@ $this->load->view('people_type');
 				</div>
 
 				<div class="form-group">
+					<label for="car_license_plate" class="col-sm-2 control-label">ทะเบียน</label>
+
+					<div class="col-sm-4">
+						<input type="text" class="form-control" id="car_license_plate" name="car_license_plate" placeholder="ทะเบียน"
+						 value="<?php echo $car_license_plate; ?>">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="car_brand" class="col-sm-2 control-label">ยี่ห้อ</label>
+
+					<div class="col-sm-4">
+						<input type="text" class="form-control" id="car_brand" name="car_brand" placeholder="ยี่ห้อ" value="<?php echo $car_brand; ?>">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="car_model" class="col-sm-2 control-label">รุ่น</label>
+
+					<div class="col-sm-4">
+						<input type="text" class="form-control" id="car_model" name="car_model" placeholder="รุ่น" value="<?php echo $car_model; ?>">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="car_color" class="col-sm-2 control-label">สี</label>
+
+					<div class="col-sm-4">
+						<input type="text" class="form-control" id="car_color" name="car_color" placeholder="สี" value="<?php echo $car_color; ?>">
+					</div>
+				</div>
+
+				<div class="form-group">
 					<label for="arrested_status" class="col-sm-2 control-label">การติดตามจับกุม</label>
 					<div class="col-sm-4">
 						<label>
